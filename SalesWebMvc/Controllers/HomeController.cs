d9603be0e71ae418d68493f4e403e22160a84bba
Iaritzza <iaritzzayara@gmail.com>
2021-08-30 21:54:49 -0300

    Pequenas alterações

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Curso C#";
+            ViewData["Aluna"] = "Iaritzza Bielinki";
 
             return View();
         }
